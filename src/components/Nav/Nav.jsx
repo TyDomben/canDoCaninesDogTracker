f6254a63487036d6afd3c0797c095391cc19b014
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2024-03-05 11:18:40 -0600

    components added to NAVBAR

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+// import { Link } from "@mui/material";
 import { useSelector } from "react-redux";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+// import Typography from "@mui/material/Typography";
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -13,46 +14,70 @@ function Nav() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          <Link to="/home" style={{ textDecoration: "none", color: "inherit" }}>
-            Can Do Canines
-          </Link>
-        </Typography>
-
-        {/* Show links depending on user role or authentication status */}
-        {!user.id ? (
-          // If there's no user logged in, show login/registration links
+        {/* ... other components */}
+        {user.id && (
+          // Only show these links if a user is logged in
           <>
-            <Button color="inherit" component={NavLink} to="/login">
-              Login
-            </Button>
-            <Button color="inherit" component={NavLink} to="/registration">
-              Register
-            </Button>
-          </>
-        ) : (
-          // If a user is logged in, show links based on their role
-          <>
-            <Button color="inherit" component={NavLink} to="/user">
-              Home
+            <Button color="inherit" component={NavLink} to="/user-home">
+              User Home
             </Button>
             {user.role === "admin" && (
+              // Only show this link for admins
               <Button color="inherit" component={NavLink} to="/admin-home">
-                Admin Home
+                Admin
+              </Button>
+            )}
+            {user.role === "raiser" && (
+              // Only show this link for raisers
+              <Button color="inherit" component={NavLink} to="/raiser-dog-page">
+                Raiser
               </Button>
             )}
             {user.role === "sitter" && (
+              // Only show this link for sitters
               <Button color="inherit" component={NavLink} to="/sitter-home">
-                Sitter Home
+                Sitter
               </Button>
             )}
-            <LogOutButton className="navLink" />
+            {/* ... other links for different roles */}
           </>
         )}
-
         <Button color="inherit" component={NavLink} to="/about">
           About
         </Button>
+        <Button color="inherit" component={NavLink} to="/data-grid">
+          Data Grid
+        </Button>
+        <Button color="inherit" component={NavLink} to="/all-dog-cards">
+          All Dog Cards
+        </Button>
+        <Button color="inherit" component={NavLink} to="/login">
+          Login
+        </Button>
+        <Button color="inherit" component={NavLink} to="/registration">
+          Register
+        </Button>
+        <Button color="inherit" component={NavLink} to="/sitter-home">
+          Sitter Home
+        </Button>
+        <Button color="inherit" component={NavLink} to="/user-home">
+          User Home
+        </Button>
+        <Button color="inherit" component={NavLink} to="/admin-home">
+          Admin Home
+        </Button>
+        <Button color="inherit" component={NavLink} to="/user-edit">
+          User Edit
+        </Button>
+        <Button color="inherit" component={NavLink} to="/user">
+          User
+        </Button>
+        <Button color="inherit" component={NavLink} to="/info">
+          Info
+        </Button>
+
+        {/* Include the logout button only if a user is logged in */}
+        {user.id && <LogOutButton className="navLink" />}
       </Toolbar>
     </AppBar>
   );
