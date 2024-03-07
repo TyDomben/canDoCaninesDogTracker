6abeeed6e3ab7169466564581b4094b60cc40b8c
Paul LeRoy <Pleroy33@gmail.com>
2024-03-06 18:20:55 -0600

    added stepper framework

--- src/components/AddDogForm/AddDogForm.jsx
+++ src/components/AddDogForm/AddDogForm.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import { Container, TextField, Button, Typography, Box, AppBar, Toolbar, IconButton, Stepper, Step, StepLabel } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useDispatch } from 'react-redux';
+
 
 
 
 const AddDogForm = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = ['Dog Information', 'Medical Information', 'Behavioral Information'];
   const [formValues, setFormValues] = useState({
-    hostName: '',
-    hostEmail: '',
     dogName: '',
     dogAge: '',
     breed: '',
@@ -37,15 +40,15 @@ const AddDogForm = () => {
     //preferences
     //lives with ?
     // environmentalPreferences: '',
-    living_with_other_dogs: '',
-    living_with_cats: '',
-    living_with_children_younger_ten: '',
+    livingWithOtherDogs: '',
+    livingWithCats: '',
+    livingWithChildrenYoungerThanTen: '',
     living_with_adults: '',
     living_with_small_animals: '',
     living_with_large_animals: '',
     behavior_with_other_dogs: '',
     behavior_with_cats: '',
-    behavior_with_cats: ''
+    behaviorWithChildren: ''
 
 
 
@@ -65,68 +68,172 @@ const AddDogForm = () => {
   };
 
   const handleSave = (event) => {
-    console.log('in postDogForm Save')
+    console.log('in postDogForm Save', formValues)
+
     // Save the data
 
       event.preventDefault();
-      // dispatch({ type: "POST_DOG", payload: formValues })
-      // history.push('/reviewemployees')
+      dispatch({ type: "POST_DOG", payload: formValues });
+      setFormValues({ 
+      dogName: '',
+      dogAge: '',
+      breed: '',
+      intact: '',
+      // vetAppointments: '',
+      foodType: '',
+      feedingFrequency: '',
+      feedingAmount: '',
+      // medicalAppointments: '',
+      medicalConditions: '',
+      surguryRecovery: '',
+      currentMedications: '',
+      inHeat: '',
+      //pottyHabitis
+      pottyFrequency: '',
+      pottyIndicators: '',
+      pottyComments: '',
+      //excercise
+      exerciseRestrictions: '',
+      exerciseEquipment: '',
+      //crating
+      cratingHabits: '',
+      //houseManners
+      houseManners: '',
+      //preferences
+      //lives with ?
+      // environmentalPreferences: '',
+      livingWithOtherDogs: '',
+      livingWithCats: '',
+      livingWithChildrenYoungerThanTen: '',
+      living_with_adults: '',
+      living_with_small_animals: '',
+      living_with_large_animals: '',
+      behavior_with_other_dogs: '',
+      behavior_with_cats: '',
+      behaviorWithChildren: ''})
+      // history.push('/')
   }
     // Show confirmation alert
-  
+    const handleNext = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+    const handleBack = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+    
 
   const handleGoBack = () => {
     // Confirm if the user wants to go back without saving
     // Redirect to home page
     history.push('/raiser-home')
   };
-
+  const renderStepContent = (step) => {
+    switch (step) {
+      case 0:
+        return (
+          <>
+            <TextField
+              fullWidth
+              margin="normal"
+              id="hostName"
+              name="hostName"
+              label="Host Name"
+              value={formValues.hostName}
+              onChange={handleChange}
+              variant="outlined"
+            />
+            {/* ... other host fields ... */}
+          </>
+        );
+      case 1:
+        return (
+          <>
+            <TextField
+              fullWidth
+              margin="normal"
+              id="dogName"
+              name="dogName"
+              label="Dog Name"
+              value={formValues.dogName}
+              onChange={handleChange}
+              variant="outlined"
+            />
+            {/* ... other dog fields ... */}
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <TextField
+              fullWidth
+              margin="normal"
+              id="vetAppointments"
+              name="vetAppointments"
+              label="Vet Appointments"
+              value={formValues.vetAppointments}
+              onChange={handleChange}
+              variant="outlined"
+            />
+            {/* ... other medical fields ... */}
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <TextField
+              fullWidth
+              margin="normal"
+              id="houseManners"
+              name="houseManners"
+              label="House Manners"
+              value={formValues.houseManners}
+              onChange={handleChange}
+              variant="outlined"
+            />
+            {/* ... other behavioral fields ... */}
+          </>
+        );
+      default:
+        return null;
+    }
+  };
   return (
     <Container maxWidth="sm">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Can Do Canines
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      {/* ... AppBar and Toolbar ... */}
       <Box component="form" noValidate autoComplete="off" sx={{ mt: 3 }}>
         <Typography variant="h5" align="center" gutterBottom>
           Add a Dog to Your Profile
         </Typography>
-        {Object.keys(formValues).map((key) => (
-          <TextField
-            key={key}
-            fullWidth
-            margin="normal"
-            id={key}
-            name={key}
-            label={key.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => str.toUpperCase())} // Converts camelCase to Title Case
-            value={formValues[key]}
-            onChange={handleChange}
-            variant="outlined"
-          />
-        ))}
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
-          <Button variant="outlined" color="secondary" onClick={handleGoBack}>
-            Go Back
-          </Button>
-          <Button variant="contained" color="primary" onClick={handleSave}>
-            Save Information
-          </Button>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        {renderStepContent(activeStep)}
+<Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+          {activeStep === 0 ? (
+            <Button variant="outlined" color="secondary" onClick={handleGoBack}>
+              Go Back
+            </Button>
+          ) : (
+            <Button variant="outlined" color="secondary" onClick={handleBack}>
+              Back
+            </Button>
+          )}
+          {activeStep === steps.length - 1 ? (
+            <Button variant="contained" color="primary" onClick={handleSave}>
+              Save Information
+            </Button>
+          ) : (
+            <Button variant="contained" color="primary" onClick={handleNext}>
+              Next
+            </Button>
+          )}
         </Box>
       </Box>
     </Container>
   );
 };
-
 export default AddDogForm;
