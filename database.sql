8774b0cc7589e0f07927a172dd8989066869cee0
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2024-03-12 15:28:21 -0500

    fixed some SQL inconsistencies, some spelling errors, alinged the children under and over ten logic to use the same verbiage, etc.

--- database.sql
+++ database.sql
@@ -16,25 +16,27 @@ CREATE TABLE IF NOT EXISTS "user" (
     "admin" BOOLEAN
 );
 
+
+
 ---------------------------------------------------------------- BEHAVIOR TABLE ----------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS "behavior" (
     "id" SERIAL PRIMARY KEY,
     "behavior_category_name" VARCHAR(255) NOT NULL
 );
 INSERT INTO "behavior" ("behavior_category_name")
-VALUES ('unknown'), ('comfortable'), ('indefferent'), ('uncomfortable');
+VALUES ('unknown'), ('comfortable'), ('indifferent'), ('uncomfortable');
 
 ---------------------------------------------------------------- EXERCISE LIMITATION TABLE ------------------------------------------------------
 
 
-CREATE TABLE IF NOT EXISTS "exercise_limitations" (
-    "id" SERIAL PRIMARY KEY,
-    "exercise_limitations" VARCHAR(255) NOT NULL
-);
-
-INSERT INTO "exercise_limitations" ("exercise_limitations")
-VALUES ('limit water'), ('limit toy play'), ('may destroy toys(watch carefully)'), ('may injest toys'),
-('plays keep away'), ('does not share toys with other dogs');
+--CREATE TABLE IF NOT EXISTS "exercise_limitations" (
+--    "id" SERIAL PRIMARY KEY,
+--    "exercise_limitations" VARCHAR(255) NOT NULL
+--);
+--
+--INSERT INTO "exercise_limitations" ("exercise_limitations")
+--VALUES ('limit water'), ('limit toy play'), ('may destroy toys(watch carefully)'), ('may ingest toys'),
+--('plays keep away'), ('does not share toys with other dogs');
 
 ---------------------------------------------------------------- EXERCISE EQUIPMENT TABLE ------------------------------------------------------
 
@@ -69,11 +71,10 @@ VALUES ('Labrador'), ('Golden Retriever'), ('Labrador Mix'), ('Golden Retriever 
 
 ---------------------------------------------------------------- In Heat Table-----------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS "in_heat"
-
-	("id" SERIAL PRIMARY KEY,
-	"condition" VARCHAR(20) NOT NULL
-	);
+CREATE TABLE IF NOT EXISTS "in_heat" (
+    "id" SERIAL PRIMARY KEY,
+    "condition" VARCHAR(20) NOT NULL
+);
 INSERT INTO "in_heat" ("condition") 
 VALUES ('yes'), ('no'), ('unknown');
 ---------------------------------------------------------------- DOGS TABLE ----------------------------------------------------------------
@@ -106,7 +107,7 @@ CREATE TABLE IF NOT EXISTS "dogs" (
     "house_manners" VARCHAR(255) NOT NULL,
     "living_with_other_dogs" BOOLEAN NOT NULL,
     "living_with_cats" BOOLEAN NOT NULL,
-    "living_with_children_ten_and_up" BOOLEAN NOT NULL,
+    "living_with_children_older_ten" BOOLEAN NOT NULL,
     "living_with_children_younger_ten" BOOLEAN NOT NULL,
     "living_with_adults" BOOLEAN NOT NULL,
     "living_with_small_animals" BOOLEAN NOT NULL,
@@ -125,9 +126,10 @@ CREATE TABLE IF NOT EXISTS "dogs" (
     
 );
 
+
 INSERT INTO "dogs" (
 	"user_id",
-    "name",
+    "dog_name",
     "age",
     "breed",
     "spayed_neutered",
@@ -141,7 +143,12 @@ INSERT INTO "dogs" (
     "in_heat",
     "potty_routine",
     "potty_habits_notes",
-    "exercise_limitations",
+    "limit_water",
+    "limit_toy_play",
+    "watch_carefully",
+    "ingest_toys",
+    "keep_away",
+    "shares_toys",
     "exercise_equipment",
     "crate_manners",
     "house_manners",
@@ -155,10 +162,9 @@ INSERT INTO "dogs" (
     "behavior_with_other_dogs",
     "behavior_with_cats",
     "behavior_with_children")
-VALUES ('1','Loki', '2', '1', true, '1', '1 cup', '3', '6 am', 'none', false, 'none', '1', 'every 3 hours', 'takes a long time', '1', '1', 'feels safe in crate', 'gets on the couch', true, true, false, false, true, true, true, '1', '1', '1');
+VALUES ('1','Loki', '2', '1', true, '1', '1 cup', '3', '6 am', 'none', false, 'none', '1', 'every 3 hours', 'takes a long time', true, true, true, true, true, true, '1', 'feels safe in crate', 'gets on the couch', true, true, false, false, true, true, true, '1', '1', '1');
 
 ---------------------------------------------------------------- DOG HOSTING TABLE ----------------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS "dog_hosting" (
     "id" SERIAL PRIMARY KEY,
     "dog_id" INT NOT NULL,
@@ -168,8 +174,8 @@ CREATE TABLE IF NOT EXISTS "dog_hosting" (
     "date_comments" VARCHAR(255) NOT NULL,
     "appointments" VARCHAR(255) NOT NULL,
     "status" VARCHAR(50) NOT NULL,
-    FOREIGN KEY ("dog_id") REFERENCES "dogs"("id"),
-    FOREIGN KEY ("user_id") REFERENCES "user"("id")
+    FOREIGN KEY ("dog_id") REFERENCES "dogs"("id") ON DELETE CASCADE,
+    FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE
 );
 
 INSERT INTO "dog_hosting" (
@@ -181,15 +187,14 @@ INSERT INTO "dog_hosting" (
     "appointments",
     "status"
 )
-VALUES('1', '1', '4-1-2024', '4-7-2024', 'Vacation to Hawaii', 'no appointments', 'not confirmed');
+VALUES(3, 1, '2024-04-01', '2024-04-07', 'Vacation to Hawaii', 'no appointments', 'not confirmed');
 --admin_id is not doing anything.. is there supposed to be an admin table with confirm and deny = 1 or 2 --
-
 -------------------------------- QUERIES --------------------------
 
 ------------ GET --------------
 SELECT
 	"dogs"."user_id",
-    "dogs"."name", 
+    "dogs"."dog_name", 
     "dogs"."age", 
     "dogs"."breed", 
     "dogs"."spayed_neutered", 
@@ -203,14 +208,13 @@ SELECT
     "dogs"."in_heat", 
     "dogs"."potty_routine", 
     "dogs"."potty_habits_notes", 
-    "exercise_limitations"."exercise_limitations", 
     "exercise_equipment"."exercise_equipment", 
     "dogs"."crate_manners", 
     "dogs"."house_manners", 
     "dogs"."living_with_other_dogs", 
     "dogs"."living_with_cats", 
-    "dogs"."living_with_children_>_ten", 
-    "dogs"."living_with_children_<_ten", 
+    "dogs"."living_with_children_older_ten", 
+    "dogs"."living_with_children_younger_ten", 
     "dogs"."living_with_adults", 
     "dogs"."living_with_small_animals", 
     "dogs"."living_with_large_animals", 
@@ -221,8 +225,8 @@ FROM
     "dogs"
 JOIN 
 	"dog_hosting" ON "dogs"."id" = "dog_hosting"."dog_id"
-JOIN 
-    "exercise_limitations" AS "exercise_limitations" ON "dogs"."exercise_limitations" = "exercise_limitations"."id"
+--JOIN 
+--    "exercise_limitations" AS "exercise_limitations" ON "dogs"."exercise_limitations" = "exercise_limitations"."id"
 JOIN 
     "exercise_equipment" AS "exercise_equipment" ON "dogs"."exercise_equipment" = "exercise_equipment"."id"
 JOIN 
@@ -239,7 +243,7 @@ WHERE
 
 INSERT INTO "dogs" (
                 "user_id",
-                "name",
+                "dog_name",
                 "age",
                 "breed",
                 "spayed_neutered",
@@ -253,7 +257,6 @@ INSERT INTO "dogs" (
                 "in_heat",
                 "potty_routine",
                 "potty_habits_notes",
-                "exercise_limitations",
                 "exercise_equipment",
                 "crate_manners",
                 "house_manners",
@@ -266,6 +269,13 @@ INSERT INTO "dogs" (
                 "living_with_large_animals",
                 "behavior_with_other_dogs",
                 "behavior_with_cats",
-                "behavior_with_children")
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29);
+                "behavior_with_children",
+                "limit_water",
+                "limit_toy_play",
+                "watch_carefully",
+                "ingest_toys",
+                "keep_away",
+                "shares_toys")
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34);
+-- 34 values?
 
