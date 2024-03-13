b16a9e931b70dd4284469124cf3d8e519de538b1
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2024-03-13 10:50:17 -0500

    fixed dog-name naming convention to match SQL

--- database.sql
+++ database.sql
@@ -3,8 +3,6 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 ---------------------------------------------------------------- USER TABLE ----------------------------------------------------------------
-
-
 CREATE TABLE IF NOT EXISTS "user" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(255),
@@ -16,19 +14,21 @@ CREATE TABLE IF NOT EXISTS "user" (
     "admin" BOOLEAN
 );
 
-
-
 ---------------------------------------------------------------- BEHAVIOR TABLE ----------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS "behavior" (
     "id" SERIAL PRIMARY KEY,
     "behavior_category_name" VARCHAR(255) NOT NULL
 );
-INSERT INTO "behavior" ("behavior_category_name")
-VALUES ('unknown'), ('comfortable'), ('indifferent'), ('uncomfortable');
+
+INSERT INTO
+    "behavior" ("behavior_category_name")
+VALUES
+    ('unknown'),
+    ('comfortable'),
+    ('indifferent'),
+    ('uncomfortable');
 
 ---------------------------------------------------------------- EXERCISE LIMITATION TABLE ------------------------------------------------------
-
-
 --CREATE TABLE IF NOT EXISTS "exercise_limitations" (
 --    "id" SERIAL PRIMARY KEY,
 --    "exercise_limitations" VARCHAR(255) NOT NULL
@@ -37,48 +37,67 @@ VALUES ('unknown'), ('comfortable'), ('indifferent'), ('uncomfortable');
 --INSERT INTO "exercise_limitations" ("exercise_limitations")
 --VALUES ('limit water'), ('limit toy play'), ('may destroy toys(watch carefully)'), ('may ingest toys'),
 --('plays keep away'), ('does not share toys with other dogs');
-
 ---------------------------------------------------------------- EXERCISE EQUIPMENT TABLE ------------------------------------------------------
-
-
 CREATE TABLE IF NOT EXISTS "exercise_equipment" (
     "id" SERIAL PRIMARY KEY,
     "exercise_equipment" VARCHAR(255) NOT NULL
 );
 
-INSERT INTO "exercise_equipment" ("exercise_equipment")
-VALUES ('gentle leader'), ('halti head caller'), ('collar only(unless pulling)'), ('no pull front clip harness'), ('walks not recommended for exercise');
+INSERT INTO
+    "exercise_equipment" ("exercise_equipment")
+VALUES
+    ('gentle leader'),
+    ('halti head caller'),
+    ('collar only(unless pulling)'),
+    ('no pull front clip harness'),
+    ('walks not recommended for exercise');
 
 ---------------------------------------------------------------- Food Type ------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS "food_type" (
     "id" SERIAL PRIMARY KEY,
     "food_type" VARCHAR(255) NOT NULL
 );
 
-INSERT INTO "food_type" ("food_type")
-VALUES ('Purina Pro Plan Large Breed Puppy'), ('Purina Pro Plan Large Breed Adult'), ('Purina Pro Plan Sensitive Skin and Stomach'), ('Purina Pro Plan Sport 30/20'), ('Other');
+INSERT INTO
+    "food_type" ("food_type")
+VALUES
+    ('Purina Pro Plan Large Breed Puppy'),
+    ('Purina Pro Plan Large Breed Adult'),
+    ('Purina Pro Plan Sensitive Skin and Stomach'),
+    ('Purina Pro Plan Sport 30/20'),
+    ('Other');
 
 ---------------------------------------------------------------- Breed ------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS "breed" (
     "id" SERIAL PRIMARY KEY,
     "breed" VARCHAR(255) NOT NULL
 );
 
-INSERT INTO "breed" ("breed")
-VALUES ('Labrador'), ('Golden Retriever'), ('Labrador Mix'), ('Golden Retriever Mix'), ('Poodle/Poodle Mix'), ('Collie'), ('I Dont Know');
+INSERT INTO
+    "breed" ("breed")
+VALUES
+    ('Labrador'),
+    ('Golden Retriever'),
+    ('Labrador Mix'),
+    ('Golden Retriever Mix'),
+    ('Poodle/Poodle Mix'),
+    ('Collie'),
+    ('I Dont Know');
 
 ---------------------------------------------------------------- In Heat Table-----------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS "in_heat" (
     "id" SERIAL PRIMARY KEY,
     "condition" VARCHAR(20) NOT NULL
 );
-INSERT INTO "in_heat" ("condition") 
-VALUES ('yes'), ('no'), ('unknown');
----------------------------------------------------------------- DOGS TABLE ----------------------------------------------------------------
 
+INSERT INTO
+    "in_heat" ("condition")
+VALUES
+    ('yes'),
+    ('no'),
+    ('unknown');
+
+---------------------------------------------------------------- DOGS TABLE ----------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS "dogs" (
     "id" SERIAL PRIMARY KEY,
     "user_id" INT REFERENCES "user"("id") NOT NULL,
@@ -101,7 +120,7 @@ CREATE TABLE IF NOT EXISTS "dogs" (
     "watch_carefully" BOOLEAN NOT NULL,
     "ingest_toys" BOOLEAN NOT NULL,
     "keep_away" BOOLEAN NOT NULL,
-    "shares_toys" BOOLEAN NOT NULL,    
+    "shares_toys" BOOLEAN NOT NULL,
     "exercise_equipment" INT NOT NULL,
     "crate_manners" VARCHAR(255) NOT NULL,
     "house_manners" VARCHAR(255) NOT NULL,
@@ -123,46 +142,82 @@ CREATE TABLE IF NOT EXISTS "dogs" (
     FOREIGN KEY ("food_type") REFERENCES "food_type"("id"),
     FOREIGN KEY ("breed") REFERENCES "breed"("id"),
     FOREIGN KEY ("in_heat") REFERENCES "in_heat"("id")
-    
 );
 
-
-INSERT INTO "dogs" (
-	"user_id",
-    "dog_name",
-    "age",
-    "breed",
-    "spayed_neutered",
-    "food_type",
-    "food_amount",
-    "meals_per_day",
-    "eating_times",
-    "medical_conditions",
-    "recovering_from_surgery",
-    "medications",
-    "in_heat",
-    "potty_routine",
-    "potty_habits_notes",
-    "limit_water",
-    "limit_toy_play",
-    "watch_carefully",
-    "ingest_toys",
-    "keep_away",
-    "shares_toys",
-    "exercise_equipment",
-    "crate_manners",
-    "house_manners",
-    "living_with_other_dogs",
-    "living_with_cats",
-    "living_with_children_older_ten",
-    "living_with_children_younger_ten",
-    "living_with_adults",
-    "living_with_small_animals",
-    "living_with_large_animals",
-    "behavior_with_other_dogs",
-    "behavior_with_cats",
-    "behavior_with_children")
-VALUES ('1','Loki', '2', '1', true, '1', '1 cup', '3', '6 am', 'none', false, 'none', '1', 'every 3 hours', 'takes a long time', true, true, true, true, true, true, '1', 'feels safe in crate', 'gets on the couch', true, true, false, false, true, true, true, '1', '1', '1');
+INSERT INTO
+    "dogs" (
+        "user_id",
+        "dog_name",
+        "age",
+        "breed",
+        "spayed_neutered",
+        "food_type",
+        "food_amount",
+        "meals_per_day",
+        "eating_times",
+        "medical_conditions",
+        "recovering_from_surgery",
+        "medications",
+        "in_heat",
+        "potty_routine",
+        "potty_habits_notes",
+        "limit_water",
+        "limit_toy_play",
+        "watch_carefully",
+        "ingest_toys",
+        "keep_away",
+        "shares_toys",
+        "exercise_equipment",
+        "crate_manners",
+        "house_manners",
+        "living_with_other_dogs",
+        "living_with_cats",
+        "living_with_children_older_ten",
+        "living_with_children_younger_ten",
+        "living_with_adults",
+        "living_with_small_animals",
+        "living_with_large_animals",
+        "behavior_with_other_dogs",
+        "behavior_with_cats",
+        "behavior_with_children"
+    )
+VALUES
+    (
+        '1',
+        'Loki',
+        '2',
+        '1',
+        true,
+        '1',
+        '1 cup',
+        '3',
+        '6 am',
+        'none',
+        false,
+        'none',
+        '1',
+        'every 3 hours',
+        'takes a long time',
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        '1',
+        'feels safe in crate',
+        'gets on the couch',
+        true,
+        true,
+        false,
+        false,
+        true,
+        true,
+        true,
+        '1',
+        '1',
+        '1'
+    );
 
 ---------------------------------------------------------------- DOG HOSTING TABLE ----------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS "dog_hosting" (
@@ -178,104 +233,145 @@ CREATE TABLE IF NOT EXISTS "dog_hosting" (
     FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE
 );
 
-INSERT INTO "dog_hosting" (
-    "dog_id",
-    "user_id",
-    "start_date",
-    "end_date",
-    "date_comments",
-    "appointments",
-    "status"
-)
-VALUES(3, 1, '2024-04-01', '2024-04-07', 'Vacation to Hawaii', 'no appointments', 'not confirmed');
+INSERT INTO
+    "dog_hosting" (
+        "dog_id",
+        "user_id",
+        "start_date",
+        "end_date",
+        "date_comments",
+        "appointments",
+        "status"
+    )
+VALUES
+(
+        3,
+        1,
+        '2024-04-01',
+        '2024-04-07',
+        'Vacation to Hawaii',
+        'no appointments',
+        'not confirmed'
+    );
+
 --admin_id is not doing anything.. is there supposed to be an admin table with confirm and deny = 1 or 2 --
 -------------------------------- QUERIES --------------------------
-
 ------------ GET --------------
 SELECT
-	"dogs"."user_id",
-    "dogs"."dog_name", 
-    "dogs"."age", 
-    "dogs"."breed", 
-    "dogs"."spayed_neutered", 
-    "dogs"."food_type", 
-    "dogs"."food_amount", 
-    "dogs"."meals_per_day", 
-    "dogs"."eating_times", 
-    "dogs"."medical_conditions", 
-    "dogs"."recovering_from_surgery", 
-    "dogs"."medications", 
-    "dogs"."in_heat", 
-    "dogs"."potty_routine", 
-    "dogs"."potty_habits_notes", 
-    "exercise_equipment"."exercise_equipment", 
-    "dogs"."crate_manners", 
-    "dogs"."house_manners", 
-    "dogs"."living_with_other_dogs", 
-    "dogs"."living_with_cats", 
-    "dogs"."living_with_children_older_ten", 
-    "dogs"."living_with_children_younger_ten", 
-    "dogs"."living_with_adults", 
-    "dogs"."living_with_small_animals", 
-    "dogs"."living_with_large_animals", 
+    "dogs"."user_id",
+    "dogs"."dog_name",
+    "dogs"."age",
+    "dogs"."breed",
+    "dogs"."spayed_neutered",
+    "dogs"."food_type",
+    "dogs"."food_amount",
+    "dogs"."meals_per_day",
+    "dogs"."eating_times",
+    "dogs"."medical_conditions",
+    "dogs"."recovering_from_surgery",
+    "dogs"."medications",
+    "dogs"."in_heat",
+    "dogs"."potty_routine",
+    "dogs"."potty_habits_notes",
+    "exercise_equipment"."exercise_equipment",
+    "dogs"."crate_manners",
+    "dogs"."house_manners",
+    "dogs"."living_with_other_dogs",
+    "dogs"."living_with_cats",
+    "dogs"."living_with_children_older_ten",
+    "dogs"."living_with_children_younger_ten",
+    "dogs"."living_with_adults",
+    "dogs"."living_with_small_animals",
+    "dogs"."living_with_large_animals",
     "behavior_dog"."behavior_category_name" AS "behavior_with_other_dogs",
     "behavior_cat"."behavior_category_name" AS "behavior_with_cats",
     "behavior_child"."behavior_category_name" AS "behavior_with_children"
-FROM 
+FROM
     "dogs"
-JOIN 
-	"dog_hosting" ON "dogs"."id" = "dog_hosting"."dog_id"
---JOIN 
---    "exercise_limitations" AS "exercise_limitations" ON "dogs"."exercise_limitations" = "exercise_limitations"."id"
-JOIN 
-    "exercise_equipment" AS "exercise_equipment" ON "dogs"."exercise_equipment" = "exercise_equipment"."id"
-JOIN 
-    "behavior" AS "behavior_dog" ON "dogs"."behavior_with_other_dogs" = "behavior_dog"."id"
-JOIN 
-    "behavior" AS "behavior_cat" ON "dogs"."behavior_with_cats" = "behavior_cat"."id"
-JOIN 
-    "behavior" AS "behavior_child" ON "dogs"."behavior_with_children" = "behavior_child"."id"
+    JOIN "dog_hosting" ON "dogs"."id" = "dog_hosting"."dog_id"
+    JOIN "exercise_equipment" AS "exercise_equipment" ON "dogs"."exercise_equipment" = "exercise_equipment"."id"
+    JOIN "behavior" AS "behavior_dog" ON "dogs"."behavior_with_other_dogs" = "behavior_dog"."id"
+    JOIN "behavior" AS "behavior_cat" ON "dogs"."behavior_with_cats" = "behavior_cat"."id"
+    JOIN "behavior" AS "behavior_child" ON "dogs"."behavior_with_children" = "behavior_child"."id"
 WHERE
-	"dog_hosting"."user_id" = 4;
+    "dog_hosting"."user_id" = 4;
+
 ;
 
 ------------ POST --------------
+INSERT INTO
+    "dogs" (
+        "user_id",
+        "dog_name",
+        "age",
+        "breed",
+        "spayed_neutered",
+        "food_type",
+        "food_amount",
+        "meals_per_day",
+        "eating_times",
+        "medical_conditions",
+        "recovering_from_surgery",
+        "medications",
+        "in_heat",
+        "potty_routine",
+        "potty_habits_notes",
+        "exercise_equipment",
+        "crate_manners",
+        "house_manners",
+        "living_with_other_dogs",
+        "living_with_cats",
+        "living_with_children_older_ten",
+        "living_with_children_younger_ten",
+        "living_with_adults",
+        "living_with_small_animals",
+        "living_with_large_animals",
+        "behavior_with_other_dogs",
+        "behavior_with_cats",
+        "behavior_with_children",
+        "limit_water",
+        "limit_toy_play",
+        "watch_carefully",
+        "ingest_toys",
+        "keep_away",
+        "shares_toys"
+    )
+VALUES
+    (
+        $ 1,
+        $ 2,
+        $ 3,
+        $ 4,
+        $ 5,
+        $ 6,
+        $ 7,
+        $ 8,
+        $ 9,
+        $ 10,
+        $ 11,
+        $ 12,
+        $ 13,
+        $ 14,
+        $ 15,
+        $ 16,
+        $ 17,
+        $ 18,
+        $ 19,
+        $ 20,
+        $ 21,
+        $ 22,
+        $ 23,
+        $ 24,
+        $ 25,
+        $ 26,
+        $ 27,
+        $ 28,
+        $ 29,
+        $ 30,
+        $ 31,
+        $ 32,
+        $ 33,
+        $ 34
+    );
 
-INSERT INTO "dogs" (
-                "user_id",
-                "dog_name",
-                "age",
-                "breed",
-                "spayed_neutered",
-                "food_type",
-                "food_amount",
-                "meals_per_day",
-                "eating_times",
-                "medical_conditions",
-                "recovering_from_surgery",
-                "medications",
-                "in_heat",
-                "potty_routine",
-                "potty_habits_notes",
-                "exercise_equipment",
-                "crate_manners",
-                "house_manners",
-                "living_with_other_dogs",
-                "living_with_cats",
-                "living_with_children_older_ten",
-                "living_with_children_younger_ten",
-                "living_with_adults",
-                "living_with_small_animals",
-                "living_with_large_animals",
-                "behavior_with_other_dogs",
-                "behavior_with_cats",
-                "behavior_with_children",
-                "limit_water",
-                "limit_toy_play",
-                "watch_carefully",
-                "ingest_toys",
-                "keep_away",
-                "shares_toys")
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34);
 -- 34 values?
-
