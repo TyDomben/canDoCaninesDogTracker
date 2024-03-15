6803ccf5d8fa81ef4597c98e63314a8631bd47dc
Paul LeRoy <Pleroy33@gmail.com>
2024-03-15 09:35:04 -0500

    fixed query in raiserDog Router

--- server/routes/raiserDog.router.js
+++ server/routes/raiserDog.router.js
@@ -12,57 +12,58 @@ router.get("/", (req, res) => {
 
   let sqlText = `
   SELECT
-  "dogs"."user_id",
-  "dogs"."id" AS "dog_id",
-  "dogs"."dog_name",
-  "dogs"."age",
-  "dogs"."breed",
-  "dogs"."spayed_neutered",
-  "dogs"."food_type",
-  "dogs"."food_amount",
-  "dogs"."meals_per_day",
-  "dogs"."eating_times",
-  "dogs"."medical_conditions",
-  "dogs"."recovering_from_surgery",
-  "dogs"."medications",
-  "dogs"."in_heat",
-  "dogs"."potty_routine",
-  "dogs"."potty_habits_notes",
-  "exercise_equipment"."exercise_equipment",
-  "dogs"."crate_manners",
-  "dogs"."house_manners",
-  "dogs"."living_with_other_dogs",
-  "dogs"."living_with_cats",
-  "dogs"."living_with_children_ten_and_up",
-  "dogs"."living_with_children_younger_ten",
-  "dogs"."living_with_adults",
-  "dogs"."living_with_small_animals",
-  "dogs"."living_with_large_animals",
-  "behavior_dog"."behavior_category_name" AS "behavior_with_other_dogs",
-  "behavior_cat"."behavior_category_name" AS "behavior_with_cats",
-  "behavior_child"."behavior_category_name" AS "behavior_with_children"
- 
-FROM
-  "dogs" d
-JOIN
-  "breed" b ON d."breed" = b."id"
-JOIN
-  "food_type" ft ON d."food_type" = ft."id"
-JOIN
-  "exercise_equipment" ee ON d."exercise_equipment" = ee."id"
-JOIN
-  "in_heat" ih ON d."in_heat" = ih."id"
-JOIN
-  "behavior" beh ON d."behavior_with_other_dogs" = beh."id"
-JOIN
-  "behavior" beh_cats ON d."behavior_with_cats" = beh_cats."id"
-JOIN
-  "behavior" AS "behavior_child" ON "dogs"."behavior_with_children" = "behavior_child"."id"
-
-WHERE
-  d."user_id" = $1;
-
+    "dogs"."user_id",
+    "dogs"."id",
+    "dogs"."dog_name",
+    "dogs"."age",
+    "dogs"."breed",
+    "dogs"."spayed_neutered",
+    "dogs"."food_type",
+    "dogs"."food_amount",
+    "dogs"."meals_per_day",
+    "dogs"."eating_times",
+    "dogs"."medical_conditions",
+    "dogs"."recovering_from_surgery",
+    "dogs"."medications",
+    "dogs"."in_heat",
+    "dogs"."potty_routine",
+    "dogs"."potty_habits_notes",
+    "dogs"."limit_water",
+    "dogs"."limit_toy_play",
+    "dogs"."watch_carefully",
+    "dogs"."ingest_toys",
+    "dogs"."keep_away",
+    "dogs"."shares_toys",
+    "exercise_equipment"."exercise_equipment",
+    "dogs"."crate_manners",
+    "dogs"."house_manners",
+    "dogs"."living_with_other_dogs",
+    "dogs"."living_with_cats",
+    "dogs"."living_with_children_ten_and_up",
+    "dogs"."living_with_children_younger_ten",
+    "dogs"."living_with_adults",
+    "dogs"."living_with_small_animals",
+    "dogs"."living_with_large_animals",
+    "behavior_dog"."behavior_category_name" AS "behavior_with_other_dogs",
+    "behavior_cat"."behavior_category_name" AS "behavior_with_cats",
+    "behavior_child"."behavior_category_name" AS "behavior_with_children"
+  FROM
+    "user"
+  JOIN
+    "dogs" ON "user"."id" ="dogs"."user_id"
+  JOIN
+    "exercise_equipment" AS "exercise_equipment" ON "dogs"."exercise_equipment" = "exercise_equipment"."id"
+  JOIN
+    "behavior" AS "behavior_dog" ON "dogs"."behavior_with_other_dogs" = "behavior_dog"."id"
+  JOIN
+    "behavior" AS "behavior_cat" ON "dogs"."behavior_with_cats" = "behavior_cat"."id"
+  JOIN
+    "behavior" AS "behavior_child" ON "dogs"."behavior_with_children" = "behavior_child"."id"
+  WHERE
+    "dogs"."user_id" = $1;
 `;
+
+
 
   const sqlParams = [userId]; // Use the current user's ID as the parameter for the query
 
